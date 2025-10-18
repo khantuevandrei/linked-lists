2820c24f6c4a66f6e863a794c220f1cbe86af216
Khantuev Andrei <khantuevandrei@gmail.com>
2025-10-18 18:49:23 +0500

    Add append, prepend, size, head, tail, at methods

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
 function createLinkedList() {
   //add a new node to the end
-  const append = (value) => {};
+  const append = (value) => {
+    if (!head && !tail) {
+      tail = createNode();
+      tail.value = value;
+      head = tail;
+    } else {
+      temp = tail;
+      tail = createNode();
+      tail.value = value;
+      temp.next = tail;
+    }
+  };
   //add a new node to the start
-  const prepend = (value) => {};
+  const prepend = (value) => {
+    if (!head) {
+      head = createNode();
+      head.value = value;
+      tail = head;
+    } else {
+      temp = head;
+      head = createNode();
+      head.value = value;
+      head.next = temp;
+    }
+  };
   //return total number of nodes
-  const size = () => {};
+  const size = () => {
+    if (!head) return 0;
+    let count = 1;
+    let currentNode = head;
+    while (currentNode !== tail) {
+      currentNode = currentNode.next;
+      count++;
+    }
+    return count;
+  };
   //return the first node
-  const head = () => {};
+  let head;
+  const getHead = () => {
+    return head;
+  };
   //return the last node
-  const tail = () => {};
+  let tail;
+  const getTail = () => {
+    return tail;
+  };
   //return the node at a given index
   const at = () => {};
   //insert a new node with provided value at given index
@@ -24,12 +61,12 @@ function createLinkedList() {
   //show linked list objects as strings
   const toString = () => {};
 
-  return {};
+  return { append, prepend, size, getHead, getTail };
 }
 
 function createNode() {
   const value = null;
   const next = null;
 
-  return {};
+  return { value, next };
 }
