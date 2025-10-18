f6768c695b599e724718fc2f86e543d9e7cb4c25
Khantuev Andrei <khantuevandrei@gmail.com>
2025-10-18 17:31:56 +0500

    Create 2 factories for creating lists & nodes. Add required methods (empty)

---
+++ script.js
@@ -0,0 +1,35 @@
+function createLinkedList() {
+  //add a new node to the end
+  const append = (value) => {};
+  //add a new node to the start
+  const prepend = (value) => {};
+  //return total number of nodes
+  const size = () => {};
+  //return the first node
+  const head = () => {};
+  //return the last node
+  const tail = () => {};
+  //return the node at a given index
+  const at = () => {};
+  //insert a new node with provided value at given index
+  const insertAt = (value, index) => {};
+  //remove a node at a given index
+  const removeAt = (index) => {};
+  //remove last node from the list
+  const pop = () => {};
+  //return true if if the passed value is in the list
+  const contains = (value) => {};
+  //return the index of the node containing value
+  const find = (value) => {};
+  //show linked list objects as strings
+  const toString = () => {};
+
+  return {};
+}
+
+function createNode() {
+  const value = null;
+  const next = null;
+
+  return {};
+}
